a37180f7109264be82430f0f48bda4f8fe9849cb
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2014-03-07 16:15:01 +0000

    Friday 7th Mar 2014 - Final amends v3

--- category.php
+++ category.php
@@ -141,7 +141,7 @@ $page_icon = get_field('page_icon', $news_page->ID);
 			
 			<?php else: ?>
 			<h3 class="text-center">Sorry</h3>
-			<p class="text-center">There is no news for <?php single_cat_title(); ?> at the moment.</p>
+			<p class="text-center">There is no <?php single_cat_title(); ?> at the moment.</p>
 			<?php endif; ?>
 			
 		</div><!-- End Col -->
